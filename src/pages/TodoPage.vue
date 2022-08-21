656958323f23453c142abfd86fe68c517bceaecd
Slick Jurgen <slick.jurgen@googlemail.com>
2022-08-21 20:03:17 +0200

    Added connection to slowhand backend

--- src/pages/TodoPage.vue
+++ src/pages/TodoPage.vue
@@ -33,7 +33,7 @@
       <q-item
         v-for="(task, index) in tasks"
         :key="task.title" 
-        @click="task.done = !task.done"
+        @click="markTaskDone(index)"
         clickable
         :class="{ 'done bg-blue-1' : task.done }"
         v-ripple>
@@ -76,6 +76,7 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
 
 export default defineComponent({
   data() {
@@ -97,7 +98,87 @@ export default defineComponent({
       ]
     } 
   },
+
+  created() {
+    // fetch on init
+    this.fetchData()
+  },
   methods: {
+    fetchData() {
+      fetch('http://localhost:10245/graphql/todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          query: "{ allTask{_id, title, done} }",
+          //variables: {
+          //  now: new Date().toISOString(),
+          //},
+        }),
+      })
+      //  .then((res) => res.json())
+      //  .then((result) => console.log(result));
+
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data);
+          //data.data.allTask.forEach(function (task, index) {
+          for (let task of data.data.allTask) {
+            console.log(task);
+            this.tasks.push({
+              id: task._id,
+              title: task.title,
+              done: task.done?task.done:false
+            });
+          }
+          //});
+        })
+        //.then(data =>  this.tasks.push({
+        //    title: data.data.allTask[0].title,
+        //    done: false
+        //}))
+
+    },
+    saveData(task) {
+      console.log("trying to save task...");
+      fetch('http://localhost:10245/data/mutate/todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: '{"mutations":[{"createOrReplace":{"_id":"' + task.id + 
+          '", "_type":"task", "title":"' + task.title + 
+          '", "done":' + task.done + '}}]}',
+      })        
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => console.log(data))
+    },
+    deleteData(task) {
+      console.log("trying to remove task...");
+      fetch('http://localhost:10245/data/mutate/todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: '{"mutations":[{"delete":{"_id":"' + task.id + '"}}]}',
+      })        
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => console.log(data))
+    },
+    markTaskDone(index) {
+      this.tasks[index].done = !this.tasks[index].done
+      this.saveData(this.tasks[index])
+    },
     deleteTask(index) {
       this.$q.dialog({
         title: 'Confirm',
@@ -105,15 +186,16 @@ export default defineComponent({
         cancel: true,
         persistent: true
       }).onOk(() => {
+        this.deleteData(this.tasks[index])
         this.tasks.splice(index, 1)
         this.$q.notify('Task deleted')
       })
     },
     addTask() {
-      this.tasks.push({
-        title: this.newTask,
-        done: false
-      })
+      let task = {id: uuidv4(), title: this.newTask, done: false}
+      this.tasks.push(task)
+// save to db
+      this.saveData(task)
       this.newTask = ''
     }
   }
